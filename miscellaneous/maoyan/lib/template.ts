37f6e2cea39ae2f324d92b7cf4b3fa1bc2659a61
TortoiseGit <TortoiseGit@qq.com>
2020-10-15 10:57:50 +0800

    猫眼的列表

---
+++ miscellaneous/maoyan/lib/template.ts
@@ -0,0 +1,19 @@
+interface IMovie {
+    img: string;
+    title: string;
+    content: string;
+}
+
+type IMovieListFunc = (movie: IMovie) => string;
+const getMovieListTemplate :IMovieListFunc = (movie) => `
+    <a href="javascript:" class="weui-media-box weui-media-box_appmsg">
+        <div class="weui-media-box__hd" id="weui-media-box__hd">
+               <img class="weui-media-box__thumb" id="weui-media-box__thumb" src="${movie.img}" alt="">
+        </div>
+        <div class="weui-media-box__bd">
+            <h4 class="weui-media-box__title">${movie.title}</h4>
+            <p class="weui-media-box__desc">${movie.content}</p>
+        </div>
+        <span href="javascript:" class="weui-btn weui-btn_mini weui-btn_primary" id="weui-btn">购票</span>
+    </a>
+`
