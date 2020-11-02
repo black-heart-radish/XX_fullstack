df0867dcd176d5f35d40777a96a1ef9152d3e436
TortoiseGit <2634893889@qq.com>
2020-11-02 17:18:37 +0800

    this

---
+++ snail/this/index.js
@@ -0,0 +1,25 @@
+// function identify() {
+//     return this.name.toUpperCase();
+// }
+
+// function sepak() {
+//     var greeting = 'Hello , I am ' + identify.call(this);
+//     console.log(greeting);
+// }
+
+// var me = {
+//     name: 'woniu'
+// }
+
+// var you = {
+//     name: 'yangbao'
+// }
+
+// sepak.call(me);
+
+
+
+
+
+
+
