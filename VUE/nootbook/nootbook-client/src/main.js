ab10f6adeb095c68f3394fa50c8416a4d03d11c6
TortoiseGit <2634893889@qq.com>
2020-12-16 10:34:16 +0800

    nootbook

---
+++ VUE/nootbook/nootbook-client/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'amfe-flexible'
+import { Swipe, SwipeItem, Toast} from 'vant';
+import 'vant/lib/index.css';
+import axios from 'axios';
+import util from './assets/js/util'
+
+Vue.use(Swipe).use(SwipeItem).use(Toast).use(axios);
+
+Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
+Vue.prototype.$util = util
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
